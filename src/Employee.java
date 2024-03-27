ed21211d4bf2d0657878aa303b51d24fca7ae7fa
squid <sqids9s0@gmail.com>
2024-03-27 14:03:05 +0300

    course paper 1.1

---
+++ src/Employee.java
@@ -0,0 +1,54 @@
+import java.util.Objects;
+
+public class Employee {
+    private static int idCounter = 1;
+    private int id;
+    private String fullName;
+    private int department;
+    private double salary;
+
+    public Employee(String fullName, int department, double salary) {
+        this.id = idCounter++;
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee[] employees = new Employee[10];
+        employees[0] = new Employee("Иванов Иван Иванович", 1, 50000);
+        employees[1] = new Employee("Петров Петр Петрович", 2, 60000);
+        employees[2] = new Employee("Сидоров Сидор Сидорович", 3, 55000);
+
+        printAllEmployees(employees);
+    }
+
+    public static void printAllEmployees(Employee[] employees) {
+        for (Employee employee : employees) {
+            if (employee != null) {
+                System.out.println(employee);
+            }
+        }
     }
 }
